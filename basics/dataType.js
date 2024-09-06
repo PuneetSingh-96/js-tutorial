ce48e7f0a102c34438a5c69b2639012e8a9d3295
PuneetSingh-96 <152212604+PuneetSingh-96@users.noreply.github.com>
2024-09-06 18:07:56 +0000

    datatype

---
+++ basics/dataType.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as new version
+
+//alert("hello") //used in node js not in browser
+
+let name ="leo"  //string
+let age = 18     //num
+let ishuman = true   //boolean true or false
+let state;  //undefined
+
+//null-> standalone value
+//undefined -> variable without any value
+
+//symbol-> unique
+
+//object
+
+console.log(typeof "leo");
+console.log(typeof age);
+console.log(typeof null);  //object
---
+++ basics/first.js
@@ -0,0 +1,5 @@
+console.log("hello javascript !")
+
+//useful documentation website
+//MDN
+//tc39.es - ecmaScript (rule)
