c61485ce6302dce649978f3240d8e50f68b7b66a
PuneetSingh-96 <152212604+PuneetSingh-96@users.noreply.github.com>
2024-09-06 17:44:00 +0000

    basic concept

---
+++ 01_basics/letConstVar.js
@@ -0,0 +1,25 @@
+//const is used when you did not want the change in value further
+const accountId = 332113 
+
+//let is used to initiate variable
+let accountEmail = "leo@gooogle.com"
+
+//var is avoid to use bcs it dont know about scope{ change value in scope}
+var accPassword = "leo123"
+
+//initiate variable without any state
+accountCity = "Lucknow"
+
+//no value
+let accState;
+
+//update
+accountEmail = "my@google.com"
+accPassword = "1234"
+accountCity = "Ghaziabad"
+
+//no update
+//accountId = 234333
+
+console.log(accountId)
+console.table([accPassword,accountEmail,accountId,accountCity,accState])
