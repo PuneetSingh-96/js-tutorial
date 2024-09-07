88db909e68a8bc6b2dbfc0914f74513cfc6624d0
PuneetSingh-96 <152212604+PuneetSingh-96@users.noreply.github.com>
2024-09-07 10:36:43 +0000

    converstion Oprations

---
+++ basics/3_converstionOpr.js
@@ -0,0 +1,39 @@
+// let mark1 = 96
+// console.log(typeof (mark1));
+
+// let mark2 = "98";
+// console.log(typeof (mark2))
+
+// //converting mark2(string) into number
+// let newMark2 = Number(mark2)  //to convert in string we use String in place of Number
+// console.log(typeof (newMark2))
+
+// let mark3 = null
+// console.log(typeof (mark3))
+
+// let mark4 = true
+// console.log(typeof (mark4))
+
+// let mark5 = undefined
+// console.log(typeof (mark5))
+
+// //"33"=>33 Number
+// //33 => "33" String
+// //"33aa3" => NaN
+
+// let isLoggedIn = 1
+// let isLoggedOff = 0
+
+// console.log(Boolean (isLoggedIn))
+// console.log(Boolean (isLoggedOff))
+
+// let on = "on"  //true
+// console.log(Boolean (on))
+
+// let off = ""   //false
+// console.log(Boolean (off))
+
+let number = 33
+let strNumber = String(number)
+console.log(strNumber)
+console.log(typeof (strNumber))
