23c384ee4ea9d7f71a949b91c2a0fb608b0ad51c
PuneetSingh-96 <152212604+PuneetSingh-96@users.noreply.github.com>
2024-09-08 14:28:07 +0000

    comparison

---
+++ basics/4_Comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+// console.log(3>=2)
+// console.log(2==1)
+// console,log(2!=1)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);  //true
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//for strict checking we use ===
+
+console.log("2" === 2)
