e16a622cafa02860d0a16b5660734e1d99fc012f
PuneetSingh-96 <152212604+PuneetSingh-96@users.noreply.github.com>
2024-09-11 12:39:16 +0000

    Basic Array

---
+++ basics2/arrays.js
@@ -0,0 +1,45 @@
+//Arrays
+
+const name = ["joe ", "tom"]
+
+const myArrays = [0, 1, 2, 3, 4, 5, " hero", true ]
+//mix datatype
+//resizable
+
+//search
+// console.log(myArrays[0])
+
+//Arrays method
+
+// myArrays.push(4) //add
+// console.log(myArrays)
+
+// myArrays.pop()//remove last value
+// console.log(myArrays)
+
+myArrays.unshift(-1)//add element in first position
+myArrays.shift() //remove first element 
+
+// console.log(myArrays.includes(9))
+// console.log(myArrays.indexOf(5))
+
+const newArr = myArrays.join()
+// console.log(newArr)
+// console.log(myArrays) 
+// console.log(typeof myArrays) 
+
+//slice and splice
+
+const Arr = [0,1,2,3,4,5,6,7,8]
+const Arr1 = Arr.slice(1,5)  //give a portion from array
+// console.log("A" , Arr )
+// console.log("B" ,Arr1)
+
+const Arr2 = Arr.splice(1,5) //remove a portion from array
+// console.log("A", Arr)
+// console.log("C", Arr2)
+
+
+
+
+
