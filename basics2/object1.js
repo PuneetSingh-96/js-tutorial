c3f15e92e3afebd2c9360983506f169a5e334d7c
PuneetSingh-96 <152212604+PuneetSingh-96@users.noreply.github.com>
2024-09-11 17:22:15 +0000

    Basic Object

---
+++ basics2/object1.js
@@ -0,0 +1,50 @@
+//OBJECT
+
+//singleton
+
+//object literals
+
+const mySymbol = Symbol("my symbol")
+
+const Jsuser = {
+    name: "raone",
+    age : 21 ,
+    email : "myemail@google.com",
+    isLoggedIn : false,
+    lastLogginDate : [ "Monday","sunday"],
+    //mySymbol : "my symbol" //string type
+    [mySymbol] : "my symbol", //symbol type
+}
+// console.log(Jsuser.email)
+// console.log(Jsuser["email"])
+// console.log(Jsuser[mySymbol])
+
+Jsuser.email = "mynewemail@gmail.com"
+// console.log(Jsuser["email"])
+
+//Object.freeze(Jsuser)  // no updation in object
+Jsuser.email = "mylatest@gmail.com"
+//console.log(Jsuser["email"]) //no updation
+
+Jsuser.greeting = function(){
+    console.log("hi !")
+}
+//console.log(Jsuser.greeting)
+console.log(Jsuser.greeting())
+
+Jsuser.greeetingtwo = function(){
+    console.log(`hello mr.${this.name} you are good learner ...`)
+}
+console.log(Jsuser.greeetingtwo())
+
+
+
+
+
+
+
+
+
+
+
+
