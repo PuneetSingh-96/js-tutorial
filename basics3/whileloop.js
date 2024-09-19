2404ed0f461dec0a8c8468e5ca1925a2f9728bc6
PuneetSingh-96 <152212604+PuneetSingh-96@users.noreply.github.com>
2024-09-19 17:56:25 +0000

    while loop in js

---
+++ basics3/whileloop.js
@@ -0,0 +1,23 @@
+//while loop
+
+let count  = 0
+while (count <= 10 ) {
+    //console.log(`count is ${count} `)
+    count ++;
+}
+
+let arr = 0;
+let myarr = [ "ironman","spiderman", "hulk",'thor'];
+while(arr < myarr.length){
+    //console.log(`value in array is ${myarr[arr]}`)
+    arr ++;
+}
+
+let score = 1;
+do {
+    console.log(`score is ${score}`)
+    score++;
+} while (score<=10);
+
+
+
